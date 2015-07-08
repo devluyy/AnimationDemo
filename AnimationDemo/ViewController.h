a58310de6b3584d610e14ff93ab06098b4cc5daf
Devluyy <yiyanglu@yeah.net>
2015-07-08 09:34:17 +0800

    text CALayer

--- AnimationDemo/ViewController.h
+++ AnimationDemo/ViewController.h
@@ -1,7 +1,7 @@
 //
 //  ViewController.h
 //  AnimationDemo
-//
+//  参考：http://www.cnblogs.com/kenshincui/p/3972100.html
 //  Created by Lyy on 15/7/7.
 //  Copyright (c) 2015年 Lyy. All rights reserved.
 //
--- AnimationDemo/ViewController.m
+++ AnimationDemo/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015年 Lyy. All rights reserved.
 //
 
+#define kViewWidth (50)
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,7 +18,76 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    [self drawMyLayer];
+    [self drawMyLayerByDelegate];
+}
+
+#pragma mark - 绘制图层
+- (void)drawMyLayer {
+    
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    
+    CALayer *layer = [[CALayer alloc] init];
+    layer.backgroundColor = [UIColor greenColor].CGColor;
+    layer.position = CGPointMake(size.width/2, size.height/2);
+    layer.bounds = CGRectMake(0, 0, kViewWidth, kViewWidth);
+    layer.cornerRadius = kViewWidth/2;
+    layer.shadowColor = [UIColor yellowColor].CGColor;
+    layer.shadowOffset = CGSizeMake(kViewWidth/20, kViewWidth/50);
+    layer.shadowOpacity = 0.9;
+    layer.anchorPoint = CGPointMake(0, 0); 
+    [self.view.layer addSublayer:layer];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    UITouch *touch = [touches anyObject];
+    CALayer *layer = [self.view.layer.sublayers lastObject];
+    CGFloat width = layer.bounds.size.width;
+    
+    width = (width == kViewWidth) ? (kViewWidth*4):kViewWidth;
+    
+    layer.bounds = CGRectMake(0, 0, width, width);
+    layer.cornerRadius = width/2;
+    layer.position = [touch locationInView:self.view];
+    layer.shadowOffset = CGSizeMake(width/20, width/50);
+
+}
+
+#pragma mark - 使用代理方法绘图
+- (void)drawMyLayerByDelegate {
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    
+    CALayer *layer = [[CALayer alloc] init];
+    layer.backgroundColor = [UIColor redColor].CGColor;
+    layer.position = CGPointMake(size.width/2, size.height/2);
+    layer.bounds = CGRectMake(0, 0, kViewWidth, kViewWidth);
+    layer.cornerRadius = kViewWidth/2;
+    layer.masksToBounds = YES;
+    //设置边框
+    layer.borderColor=[UIColor whiteColor].CGColor;
+    layer.borderWidth=2;
+    
+    layer.delegate = self;
+    [self.view.layer addSublayer:layer];
+    
+    //调用图层setNeedDisplay,否则代理方法不会被调用
+    [layer setNeedsDisplay];
+}
+
+#pragma mark 绘制图形、图像到图层，注意参数中的ctx是图层的图形上下文，其中绘图位置也是相对图层而言的
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+    CGContextSaveGState(ctx);
+    //图形上下文形变，解决图片倒立的问题
+    CGContextScaleCTM(ctx, 1, -1);
+    CGContextTranslateCTM(ctx, 0, -kViewWidth);
+    UIImage *image = [UIImage imageNamed:@"photo.png"];
+    
+    CGContextDrawImage(ctx, CGRectMake(0, 0, kViewWidth, kViewWidth), image.CGImage);
+    
+    CGContextRestoreGState(ctx);
+    
 }
 
 - (void)didReceiveMemoryWarning {
